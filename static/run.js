1494601aa364c4d8039603eefddb835236c125a1
Glitch (shine-tinted-gardenia) <none>
2021-02-28 15:10:37 +0000

    🐀🏖 Checkpoint ./static/run.js:27710111/807 ./static/index.html:27710111/782 ./static/style.css:27710111/400

--- static/run.js
+++ static/run.js
@@ -20,11 +20,19 @@ function start() {
   }
 }
 
+let removeElement = (parent, e) => parent.removeChild(document.getElementById(e));
+
 window.onload = () => {
-  document.getElementById("title").textContent = "diep.io";
+  let lerp = (a, b, x) => a + x * (b - a);
+  removeElement(document.querySelector("center"), "loadingText");
+  let html = `<div id="textInputContainer" class="text-input-div">
+    <center><h1 id="title" class="head">This is the tale of...</h1></center>
+    <input id="textInput" onkeypress="start()" class="text-input" placeholder="This is the tale of..." autofocus>
+    <input id="token" class="token-input" type="password" placeholder="Put a developer token here...">
+  </div>`;
+  document.body.innerHTML += html;
   loaded = true;
   document.body.style.backgroundImage = "url('https://cdn.glitch.com/cb589383-631b-4d6c-845a-0d18ce5a3ee3%2Fefabd7e3-e9a4-4c2b-ab67-8fa1fa315359.image.png?v=1614524041533')";
-  //document.body.style.backgroundPosition = "center";
   document.body.style.backgroundRepeat = "repeat";
   document.body.style.backgroundSize = "cover";
 };
