fba901c168b1fcf869ef601f993ec65388bbbaaf
Glitch (shine-tinted-gardenia) <none>
2021-02-28 15:21:41 +0000

    💸🌾 Checkpoint ./static/run.js:25912110/4376:27710111/771 ./static/index.html:25912110/408

--- static/run.js
+++ static/run.js
@@ -1,16 +1,16 @@
-var socket = io.connect({
-  transports: ['websocket']
-});
+var socket = io.connect({ transports: ['websocket'] });
 let storage = ["textInput", "token"];
 for (let item of storage)
   if (localStorage[item]) document.getElementById(item).value = localStorage[item];
 
 let system;
-let loaded = false;
+let resources = [];
+let resourcesLoaded = 0;
+let loaded = resources.length ? 0 : 1;
 
 function start() {
   if (event.keyCode == 13) {
-    if (!loaded) return;
+    if (loaded < 2) return;
     document.getElementById('textInput').style.display = 'none';
     document.getElementById('token').style.display = 'none';
     document.getElementById('canvas').style.display = 'block';
@@ -22,17 +22,51 @@ function start() {
 
 let removeElement = (parent, e) => parent.removeChild(document.getElementById(e));
 
+for (let { url, then } of resources) fetch(url).then(res => res.json()).then(json => {
+  resourcesLoaded ++;
+  then(json);
+  if (resourcesLoaded === resources.length) loaded ++;
+});
+
 window.onload = () => {
   let lerp = (a, b, x) => a + x * (b - a);
   removeElement(document.querySelector("center"), "loadingText");
-  let html = `<div id="textInputContainer" class="text-input-div">
-    <center><h1 id="title" class="head">This is the tale of...</h1></center>
-    <input id="textInput" onkeypress="start()" class="text-input" placeholder="This is the tale of..." autofocus>
-    <input id="token" class="token-input" type="password" placeholder="Put a developer token here...">
-  </div>`;
-  document.body.innerHTML += html;
-  loaded = true;
+  var animation=0;
+  function getWidth() {
+  return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  );
+}
+
+function getHeight() {
+  return Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.documentElement.clientHeight
+  );
+}
+  function Loop(){
+    
+    animation=lerp(animation,Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.documentElement.clientHeight
+  )/2.25,0.05);
+    document.getElementById("textInputContainer").style.top=animation+"px" 
+    window.requestAnimationFrame(Loop);
+  };
+  Loop();
+  document.getElementById("textInputContainer").display = "block"
   document.body.style.backgroundImage = "url('https://cdn.glitch.com/cb589383-631b-4d6c-845a-0d18ce5a3ee3%2Fefabd7e3-e9a4-4c2b-ab67-8fa1fa315359.image.png?v=1614524041533')";
   document.body.style.backgroundRepeat = "repeat";
   document.body.style.backgroundSize = "cover";
+  loaded ++;
 };
