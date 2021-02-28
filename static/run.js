b356c4f9d13cd2daf2e981866ebdf90051914dc2
Glitch (shine-tinted-gardenia) <none>
2021-02-28 15:43:56 +0000

    💖📒 Checkpoint ./static/run.js:27710111/513 ./static/style.css:27710111/255 ./app.js:27710111/24

--- static/run.js
+++ static/run.js
@@ -1,7 +1,5 @@
 var socket = io.connect({ transports: ['websocket'] });
-let storage = ["textInput", "token"];
-for (let item of storage)
-  if (localStorage[item]) document.getElementById(item).value = localStorage[item];
+let storage = ["textInput"];
 
 let system;
 let resources = [];
@@ -12,10 +10,9 @@ function start() {
   if (event.keyCode == 13) {
     if (loaded < 2) return;
     document.getElementById('textInput').style.display = 'none';
-    document.getElementById('token').style.display = 'none';
     document.getElementById('canvas').style.display = 'block';
     for (let item of storage) localStorage[item] = document.getElementById(item).value;
-    system = new System(document.getElementById('textInput').value, document.getElementById('token').value);
+    system = new System(document.getElementById('textInput').value, localStorage.token);
     system.drawObject.resize();
   }
 }
@@ -29,44 +26,26 @@ for (let { url, then } of resources) fetch(url).then(res => res.json()).then(jso
 });
 
 window.onload = () => {
-  let lerp = (a, b, x) => a + x * (b - a);
+  window.lerp = (a, b, x) => a + x * (b - a);
   removeElement(document.querySelector("center"), "loadingText");
-  var animation=0;
-  function getWidth() {
-  return Math.max(
-    document.body.scrollWidth,
-    document.documentElement.scrollWidth,
-    document.body.offsetWidth,
-    document.documentElement.offsetWidth,
-    document.documentElement.clientWidth
-  );
-}
-
-function getHeight() {
-  return Math.max(
-    document.body.scrollHeight,
-    document.documentElement.scrollHeight,
-    document.body.offsetHeight,
-    document.documentElement.offsetHeight,
-    document.documentElement.clientHeight
-  );
-}
-  function Loop(){
-    
-    animation=lerp(animation,Math.max(
-    document.body.scrollHeight,
-    document.documentElement.scrollHeight,
-    document.body.offsetHeight,
-    document.documentElement.offsetHeight,
-    document.documentElement.clientHeight
-  )/2.25,0.05);
-    document.getElementById("textInputContainer").style.top=animation+"px" 
-    window.requestAnimationFrame(Loop);
-  };
-  Loop();
-  document.getElementById("textInputContainer").display = "block"
   document.body.style.backgroundImage = "url('https://cdn.glitch.com/cb589383-631b-4d6c-845a-0d18ce5a3ee3%2Fefabd7e3-e9a4-4c2b-ab67-8fa1fa315359.image.png?v=1614524041533')";
   document.body.style.backgroundRepeat = "repeat";
   document.body.style.backgroundSize = "cover";
   loaded ++;
 };
+
+function onLoad() {
+  document.body.innerHTML += `
+  <center>
+    <div id="textInputContainer" position="fixed" style="width: 600px;left: calc(50% - 300px)">
+      <h1 id="title" class="head">This is the tale of...</h1>
+      <input id="textInput" onkeypress="start()" class="text-input" autofocus>
+    </div>
+  </center>`;
+  for (let item of storage)
+    if (localStorage[item]) document.getElementById(item).value = localStorage[item];
+}
+
+let loadInterval = setInterval(() => {
+  if (loaded === 2) onLoad(), clearInterval(loadInterval);
+}, 500);
